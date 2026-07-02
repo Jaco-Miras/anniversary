1f9c995554db39dd9d42f46d7503b57938ee3496
Jaco-Miras <miras.jaco@gmail.com>
2026-07-02 18:20:54 +0800

    changes

--- src/components/Memories.jsx
+++ src/components/Memories.jsx
@@ -1,11 +1,344 @@
 import React, { useState } from "react";
 
-const allImages = Object.values(
-  import.meta.glob("/public/photos/*.{jpg,JPG,jpeg,JPEG,png,PNG,webp,WEBP}", {
-    eager: true,
-    import: "default",
-  }),
-);
+import image1 from "../../public/photos/1.jpg";
+import image2 from "../../public/photos/2.jpg";
+import image3 from "../../public/photos/3.jpg";
+import image4 from "../../public/photos/4.jpg";
+import image5 from "../../public/photos/5.jpg";
+import image6 from "../../public/photos/6.jpg";
+import image7 from "../../public/photos/7.jpg";
+import image8 from "../../public/photos/8.jpg";
+import image9 from "../../public/photos/9.jpg";
+import image10 from "../../public/photos/10.jpg";
+import image11 from "../../public/photos/11.jpg";
+import image12 from "../../public/photos/12.jpg";
+import image13 from "../../public/photos/13.jpg";
+import image14 from "../../public/photos/14.jpg";
+import image15 from "../../public/photos/15.jpg";
+import image16 from "../../public/photos/16.jpg";
+import image17 from "../../public/photos/17.jpg";
+import image18 from "../../public/photos/18.jpg";
+import image19 from "../../public/photos/19.jpg";
+import image20 from "../../public/photos/20.jpg";
+import image21 from "../../public/photos/21.jpg";
+import image22 from "../../public/photos/22.jpg";
+import image23 from "../../public/photos/23.jpg";
+import image24 from "../../public/photos/24.jpg";
+import image25 from "../../public/photos/25.jpg";
+import image26 from "../../public/photos/26.jpg";
+import image27 from "../../public/photos/27.jpg";
+import image28 from "../../public/photos/28.jpg";
+import image29 from "../../public/photos/29.jpg";
+import image30 from "../../public/photos/30.jpg";
+import image31 from "../../public/photos/31.jpg";
+import image32 from "../../public/photos/32.jpg";
+import image33 from "../../public/photos/33.jpg";
+import image34 from "../../public/photos/34.jpg";
+import image35 from "../../public/photos/35.jpg";
+import image36 from "../../public/photos/36.jpg";
+import image37 from "../../public/photos/37.jpg";
+import image38 from "../../public/photos/38.jpg";
+import image39 from "../../public/photos/39.jpg";
+import image40 from "../../public/photos/40.jpg";
+import image41 from "../../public/photos/41.jpg";
+import image42 from "../../public/photos/42.jpg";
+import image43 from "../../public/photos/43.jpg";
+import image44 from "../../public/photos/44.jpg";
+import image45 from "../../public/photos/45.jpg";
+import image46 from "../../public/photos/46.jpg";
+import image47 from "../../public/photos/47.jpg";
+import image48 from "../../public/photos/48.jpg";
+import image49 from "../../public/photos/49.jpg";
+import image50 from "../../public/photos/50.jpg";
+import image51 from "../../public/photos/51.jpg";
+import image52 from "../../public/photos/52.jpg";
+import image53 from "../../public/photos/53.jpg";
+import image54 from "../../public/photos/54.jpg";
+import image55 from "../../public/photos/55.jpg";
+import image56 from "../../public/photos/56.jpg";
+import image57 from "../../public/photos/57.jpg";
+import image58 from "../../public/photos/58.jpg";
+import image59 from "../../public/photos/59.jpg";
+import image60 from "../../public/photos/60.jpg";
+import image61 from "../../public/photos/61.jpg";
+import image62 from "../../public/photos/62.jpg";
+import image63 from "../../public/photos/63.jpg";
+import image64 from "../../public/photos/64.jpg";
+import image65 from "../../public/photos/65.jpg";
+import image66 from "../../public/photos/66.jpg";
+import image67 from "../../public/photos/67.jpg";
+import image68 from "../../public/photos/68.jpg";
+import image69 from "../../public/photos/69.jpg";
+import image70 from "../../public/photos/70.jpg";
+import image71 from "../../public/photos/71.jpg";
+import image72 from "../../public/photos/72.jpg";
+import image73 from "../../public/photos/73.jpg";
+import image74 from "../../public/photos/74.jpg";
+import image75 from "../../public/photos/75.jpg";
+import image76 from "../../public/photos/76.jpg";
+import image77 from "../../public/photos/77.jpg";
+import image78 from "../../public/photos/78.jpg";
+import image79 from "../../public/photos/79.jpg";
+import image80 from "../../public/photos/80.jpg";
+import image81 from "../../public/photos/81.jpg";
+import image82 from "../../public/photos/82.jpg";
+import image83 from "../../public/photos/83.jpg";
+import image84 from "../../public/photos/84.jpg";
+import image85 from "../../public/photos/85.jpg";
+import image86 from "../../public/photos/86.jpg";
+import image87 from "../../public/photos/87.jpg";
+import image88 from "../../public/photos/88.jpg";
+import image89 from "../../public/photos/89.jpg";
+import image90 from "../../public/photos/90.jpg";
+import image91 from "../../public/photos/91.jpg";
+import image92 from "../../public/photos/92.jpg";
+import image93 from "../../public/photos/93.jpg";
+import image94 from "../../public/photos/94.jpg";
+import image95 from "../../public/photos/95.jpg";
+import image96 from "../../public/photos/96.jpg";
+import image97 from "../../public/photos/97.jpg";
+import image98 from "../../public/photos/98.jpg";
+import image99 from "../../public/photos/99.jpg";
+import image100 from "../../public/photos/100.jpg";
+import image101 from "../../public/photos/101.jpg";
+import image102 from "../../public/photos/102.jpg";
+import image103 from "../../public/photos/103.jpg";
+import image104 from "../../public/photos/104.jpg";
+import image105 from "../../public/photos/105.jpg";
+import image106 from "../../public/photos/106.jpg";
+import image107 from "../../public/photos/107.jpg";
+import image108 from "../../public/photos/108.jpg";
+import image109 from "../../public/photos/109.jpg";
+import image110 from "../../public/photos/110.jpg";
+import image111 from "../../public/photos/111.jpg";
+import image112 from "../../public/photos/112.jpg";
+import image113 from "../../public/photos/113.jpg";
+import image114 from "../../public/photos/114.jpg";
+import image115 from "../../public/photos/115.jpg";
+import image116 from "../../public/photos/116.jpg";
+import image117 from "../../public/photos/117.jpg";
+import image118 from "../../public/photos/118.jpg";
+import image119 from "../../public/photos/119.jpg";
+import image120 from "../../public/photos/120.jpg";
+import image121 from "../../public/photos/121.jpg";
+import image122 from "../../public/photos/122.jpg";
+import image123 from "../../public/photos/123.jpg";
+import image124 from "../../public/photos/124.jpg";
+import image125 from "../../public/photos/125.jpg";
+import image126 from "../../public/photos/126.jpg";
+import image127 from "../../public/photos/127.jpg";
+import image128 from "../../public/photos/128.jpg";
+import image129 from "../../public/photos/129.jpg";
+import image130 from "../../public/photos/130.jpg";
+import image131 from "../../public/photos/131.jpg";
+import image132 from "../../public/photos/132.jpg";
+import image133 from "../../public/photos/133.jpg";
+import image134 from "../../public/photos/134.jpg";
+import image135 from "../../public/photos/135.jpg";
+import image136 from "../../public/photos/136.jpg";
+import image137 from "../../public/photos/137.jpg";
+import image138 from "../../public/photos/138.jpg";
+import image139 from "../../public/photos/139.jpg";
+import image140 from "../../public/photos/140.jpg";
+import image141 from "../../public/photos/141.jpg";
+import image142 from "../../public/photos/142.jpg";
+import image143 from "../../public/photos/143.jpg";
+import image144 from "../../public/photos/144.jpg";
+import image145 from "../../public/photos/145.jpg";
+import image146 from "../../public/photos/146.jpg";
+import image147 from "../../public/photos/147.jpg";
+import image148 from "../../public/photos/148.jpg";
+import image149 from "../../public/photos/149.jpg";
+import image150 from "../../public/photos/150.jpg";
+import image151 from "../../public/photos/151.jpg";
+import image152 from "../../public/photos/152.jpg";
+import image153 from "../../public/photos/153.jpg";
+import image154 from "../../public/photos/154.jpg";
+import image155 from "../../public/photos/155.jpg";
+import image156 from "../../public/photos/156.jpg";
+import image157 from "../../public/photos/157.jpg";
+import image158 from "../../public/photos/158.jpg";
+import image159 from "../../public/photos/159.jpg";
+import image160 from "../../public/photos/160.jpg";
+import image161 from "../../public/photos/161.jpg";
+import image162 from "../../public/photos/162.jpg";
+import image163 from "../../public/photos/163.jpg";
+import image164 from "../../public/photos/164.jpg";
+import image165 from "../../public/photos/165.jpg";
+import image166 from "../../public/photos/166.jpg";
+import image167 from "../../public/photos/167.jpg";
+import image168 from "../../public/photos/168.jpg";
+
+const allImages = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35,
+  image36,
+  image37,
+  image38,
+  image39,
+  image40,
+  image41,
+  image42,
+  image43,
+  image44,
+  image45,
+  image46,
+  image47,
+  image48,
+  image49,
+  image50,
+  image51,
+  image52,
+  image53,
+  image54,
+  image55,
+  image56,
+  image57,
+  image58,
+  image59,
+  image60,
+  image61,
+  image62,
+  image63,
+  image64,
+  image65,
+  image66,
+  image67,
+  image68,
+  image69,
+  image70,
+  image71,
+  image72,
+  image73,
+  image74,
+  image75,
+  image76,
+  image77,
+  image78,
+  image79,
+  image80,
+  image81,
+  image82,
+  image83,
+  image84,
+  image85,
+  image86,
+  image87,
+  image88,
+  image89,
+  image90,
+  image91,
+  image92,
+  image93,
+  image94,
+  image95,
+  image96,
+  image97,
+  image98,
+  image99,
+  image100,
+  image101,
+  image102,
+  image103,
+  image104,
+  image105,
+  image106,
+  image107,
+  image108,
+  image109,
+  image110,
+  image111,
+  image112,
+  image113,
+  image114,
+  image115,
+  image116,
+  image117,
+  image118,
+  image119,
+  image120,
+  image121,
+  image122,
+  image123,
+  image124,
+  image125,
+  image126,
+  image127,
+  image128,
+  image129,
+  image130,
+  image131,
+  image132,
+  image133,
+  image134,
+  image135,
+  image136,
+  image137,
+  image138,
+  image139,
+  image140,
+  image141,
+  image142,
+  image143,
+  image144,
+  image145,
+  image146,
+  image147,
+  image148,
+  image149,
+  image150,
+  image151,
+  image152,
+  image153,
+  image154,
+  image155,
+  image156,
+  image157,
+  image158,
+  image159,
+  image160,
+  image161,
+  image162,
+  image163,
+  image164,
+  image165,
+  image166,
+  image167,
+  image168,
+];
 const Memories = () => {
   const [showAll, setShowAll] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
